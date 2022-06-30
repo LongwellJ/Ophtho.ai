20f3970a5971dcc37b8848cf4bb4ef8a5d57b05c
Jackel65 <100309307+Jackel65@users.noreply.github.com>
2022-06-30 11:26:45 -0400

    Update app.py

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,17 +1,30 @@
-from flask import Flask, render_template
 
+from flask import Flask, render_template, request, redirect
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+
+@app.route("/", methods=["GET", "POST"])
+def upload_image():
+
+    if request.method == "Post":
+
+        if request.files:
+
+            image = request.files["image"]
+
+            print(image)
+            
+            return redirect(request.url)
+
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
 
     app.run(debug=True)
 
-
+Model_json = "model.json"
+Model_weights = "model.h5"
 
