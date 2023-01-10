8a49e8b3aa824a528f2b5af612c4526eb690a13f
Jack Longwell <100309307+LongwellJ@users.noreply.github.com>
2023-01-10 16:00:53 -0500

    Add files via upload

---
+++ FlaskApp/static/javascript/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+
+
+    //owl carousel
+
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+
+            0:{
+                items: 1,
+                nav: false
+            },
+
+            600:{
+                items: 2,
+                nav: false
+            },
+
+            1000:{
+                items: 3,
+                nav: false
+            }
+
+        }
+
+
+    });
+});
